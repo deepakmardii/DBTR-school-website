fa30fcc51214381fe575f4f0748aa8639c02882a
Deepak Mardi <deepakmardi22@gmail.com>
2024-03-17 00:01:50 +0530

    Footer changes

--- school-website-frontend/src/components/Footer.jsx
+++ school-website-frontend/src/components/Footer.jsx
@@ -11,17 +11,20 @@ import {
 
 const Footer = () => {
   return (
-    <div className="mx-auto max-w-screen-xl space-y-8 px-4 py-16 sm:px-6 lg:space-y-16 lg:px-8">
+    <div className="mx-auto space-y-8 px-4 py-16 sm:px-6 lg:space-y-16 lg:px-20 bg-neutral-100">
       <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
-        <div className="flex">
+        <div className="flex gap-6">
           <div>
-            <img src={logo} alt="logo" />
+            <img src={logo} alt="logo" className="w-56 object-contain	" />
           </div>
-
-          <div>
-            <div>DBTR National Higher Secondary School</div>
-            <div>Virtuousness is Life</div>
-            <div>
+          <div className="pr-14">
+            <div className="font-playfair text-lg font-semibold">
+              DBTR National Higher Secondary School
+            </div>
+            <div className="text-school-blue font-normal">
+              Virtuousness is Life
+            </div>
+            <div className="py-6 font-extralight text-sm">
               Established in 1901, DBTR is situated in the temple town of
               Mayiladuthurai.
             </div>
@@ -30,8 +33,8 @@ const Footer = () => {
 
         <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:col-span-2 lg:grid-cols-3">
           <div>
-            <p className="font-medium">QUICK LINKS</p>
-            <ul className="mt-6 space-y-4 text-sm">
+            <p className="font-medium text-dark1">QUICK LINKS</p>
+            <ul className="mt-6 space-y-4 text-dark2 font-medium">
               <li>
                 <a href="#" className="transition hover:opacity-75">
                   Admissions
@@ -56,9 +59,9 @@ const Footer = () => {
           </div>
 
           <div>
-            <p className="font-medium text-gray-900">Contact</p>
+            <p className="font-medium text-dark1">CONTACT</p>
 
-            <ul className="mt-6 space-y-4 text-sm">
+            <ul className="mt-6 space-y-4 text-dark2 font-medium">
               <li>
                 <a href="#" className="transition hover:opacity-75">
                   DBTR NHSS, Mahadhana Street, Kamarajar Salai, Mayiladuthurai,
@@ -66,13 +69,13 @@ const Footer = () => {
                 </a>
               </li>
               <li>
-                <a href="#" className="transition hover:opacity-75">
+                <a href="#" className="transition hover:opacity-75 ">
                   +91.436.422.3272
                 </a>
               </li>
 
               <li>
-                <a href="#" className="transition hover:opacity-75">
+                <a href="#" className="transition hover:opacity-75 underline">
                   contact@nationalhighschool.in
                 </a>
               </li>
@@ -81,13 +84,13 @@ const Footer = () => {
 
           <div>
             <ul className="mt-6 space-y-4 text-sm">
-              <li className="transition hover:opacity-75">
+              <li className="transition hover:opacity-75 font-playfair text-2xl">
                 Big or small, you can make an impact.
               </li>
               <li>
                 <a
                   href="#"
-                  className="rounded border border-school-red bg-school-red px-10 py-3 text-sm font-medium text-white focus:outline-none focus:ring active:text-school-red flex gap-2 justify-center items-center"
+                  className="rounded border border-school-red bg-school-red px-10 py-3 text-xl font-medium text-white focus:outline-none focus:ring active:text-school-red flex gap-2 justify-center items-center"
                 >
                   Donate <Heart size={16} fill="white" />
                 </a>
@@ -98,18 +101,21 @@ const Footer = () => {
       </div>
       <hr />
       <div className="flex justify-between">
-        <div>
-          <div className="flex">
+        <div className="text-dark1 font-light">
+          <div className="flex gap-6">
             <p>&copy; DBTR 2023, All Rights Reserved</p> | <p>Sitemap</p>
           </div>
-          <div>Designed by Deepak Mardi</div>
+          <div className=" flex flex-row justify-center items-center gap-4">
+            Designed by{" "}
+            <span className="text-4xl font-extralight">Deepak Mardi</span>
+          </div>
         </div>
-        <div className="flex gap-2">
-          <FaTwitter />
-          <FaLinkedin />
-          <FaInstagram />
-          <FaFacebook />
-          <FaYoutube />
+        <div className="flex gap-6 text-school-socials text-2xl">
+          <FaTwitter className="opacity-40 hover:opacity-100" />
+          <FaLinkedin className="opacity-40 hover:opacity-100" />
+          <FaInstagram className="opacity-40 hover:opacity-100" />
+          <FaFacebook className="opacity-40 hover:opacity-100" />
+          <FaYoutube className="opacity-40 hover:opacity-100" />
         </div>
       </div>
     </div>
--- school-website-frontend/tailwind.config.js
+++ school-website-frontend/tailwind.config.js
@@ -5,6 +5,10 @@ export default {
     extend: {
       colors: {
         "school-red": "#F54E39",
+        "school-blue": "#232C95",
+        "school-socials": "#2B306A",
+        dark1: "#4A4A4A",
+        dark2: "#191919",
       },
       fontFamily: {
         playfair: ["Playfair Display", "serif"],
