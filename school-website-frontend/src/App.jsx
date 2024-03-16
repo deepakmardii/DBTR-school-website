ae416cb72cc5ff2db79a8967b9beb3d2be22fa9c
Deepak Mardi <deepakmardi22@gmail.com>
2024-03-16 16:45:05 +0530

    Adding Navbar

--- school-website-frontend/src/App.jsx
+++ school-website-frontend/src/App.jsx
@@ -1,3 +1,15 @@
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <div>
+        <Navbar />
+        <Hero />
+        <Footer />
+      </div>
+    </>
+  );
 }
---
+++ school-website-frontend/src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import logo from "../assets/logo.png";
+import { Heart } from "lucide-react";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between px-14 py-2 items-center">
+      <div>
+        <img src={logo} alt="logo" className="h-20" />
+      </div>
+      <ul className="flex gap-10">
+        <li>The school</li>
+        <li>Academics</li>
+        <li>Life @ DBTR</li>
+        <li>Contact us</li>
+      </ul>
+      <div className="flex gap-10">
+        <button className="inline-block rounded border border-school-red px-12 py-3 text-sm font-medium text-school-red focus:outline-none focus:ring active:bg-school-red">
+          CSR
+        </button>
+        <button className="rounded border border-school-red bg-school-red px-10 py-3 text-sm font-medium text-white focus:outline-none focus:ring active:text-school-red flex gap-2">
+          Donate <Heart size={20} fill="white" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
