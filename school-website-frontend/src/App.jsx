09169cb05e3f49e0e4494c10f7ca5dc4fd1dceab
Deepak Mardi <deepakmardi22@gmail.com>
2024-03-17 11:50:29 +0530

    package update and fetching data

--- school-website-frontend/src/App.jsx
+++ school-website-frontend/src/App.jsx
@@ -1,10 +1,37 @@
+import React, { useEffect } from "react";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Categories from "./components/Gallery/Category/Categories";
 import Images from "./components/Gallery/Image/Images";
+import { useDispatch } from "react-redux";
+import { setImages } from "./feature/slices/imageSlice";
+import { setCategories } from "./feature/slices/categoriesSlice";
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const categoryRes = await fetch("http://127.0.0.1:1337/api/categories");
+        const categoryData = await categoryRes.json();
+        console.log(categoryData);
+        dispatch(setCategories(categoryData.data));
+
+        const imageRes = await fetch(
+          "http://127.0.0.1:1337/api/images?populate=*"
+        );
+        const imageData = await imageRes.json();
+        console.log(imageData);
+        dispatch(setImages(imageData.data));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchData();
+  }, [dispatch]);
   return (
     <>
       <div className="font-poppins">
